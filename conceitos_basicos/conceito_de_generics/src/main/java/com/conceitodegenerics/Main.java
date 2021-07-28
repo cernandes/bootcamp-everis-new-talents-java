c1bda2e25d6278ad7d8c5a3fd8d981b61bde5da8
Claudio <cernandes.silva@gmail.com>
2021-07-27 21:40:01 -0300

    Conceitos de generic refatorando a classe No

---
+++ conceitos_basicos/conceito_de_generics/src/main/java/com/conceitodegenerics/Main.java
@@ -0,0 +1,28 @@
+package com.conceitodegenerics;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		No<String> no1 = new No<String>("conteúdo no1");
+		No<String> no2 = new No<String>("conteúdo no2");
+		no1.setProximoNo(no2);
+
+		No<String> no3 = new No<String>("conteúdo no3");
+		no2.setProximoNo(no3);
+
+		No<String> no4 = new No<String>("conteúdo no4");
+		no3.setProximoNo(no4);
+
+		System.out.println(no1);
+		System.out.println(no1.getProximoNo());
+
+		System.out.println("----------------------------");
+		System.out.println(no1);
+		System.out.println(no1.getProximoNo());
+		System.out.println(no1.getProximoNo().getProximoNo());
+		System.out.println(no1.getProximoNo().getProximoNo().getProximoNo());
+		System.out.println(no1.getProximoNo().getProximoNo().getProximoNo().getProximoNo());
+	}
+
+}
