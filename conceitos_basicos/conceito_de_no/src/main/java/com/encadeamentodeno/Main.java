0079731cdf54e845882e2f653a7cc1c2fc46256d
Claudio <cernandes.silva@gmail.com>
2021-07-27 21:14:39 -0300

    Conceito de nó e encadeamento de nó

---
+++ conceitos_basicos/conceito_de_no/src/main/java/com/encadeamentodeno/Main.java
@@ -0,0 +1,28 @@
+package com.encadeamentodeno;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		No no1 = new No("conteúdo no1");
+		No no2 = new No("conteúdo no2");
+		no1.setProximoNo(no2);
+
+		No no3 = new No("conteúdo no3");
+		no2.setProximoNo(no3);
+		
+		No no4 = new No("conteúdo no4");
+		no3.setProximoNo(no4);
+		
+		System.out.println(no1);
+		System.out.println(no1.getProximoNo());
+		
+		System.out.println("----------------------------");
+		System.out.println(no1);
+		System.out.println(no1.getProximoNo());
+		System.out.println(no1.getProximoNo().getProximoNo());
+		System.out.println(no1.getProximoNo().getProximoNo().getProximoNo());
+		System.out.println(no1.getProximoNo().getProximoNo().getProximoNo().getProximoNo());
+	}
+
+}
